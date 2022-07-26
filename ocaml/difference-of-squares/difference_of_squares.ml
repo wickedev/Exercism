488d32e09f2fc35d8328581e89189b00df6ceb51
wickedev <orange881217@gmail.com>
2022-07-26 16:48:29 +0900

    difference-of-squares 공백 추가

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -3,6 +3,6 @@ let square n = n * n
 
 let square_of_sum n = square (n * (n + 1) / 2)
 
-let sum_of_squares n = (n * (n + 1) * (2 * n + 1)) /6
+let sum_of_squares n = (n * (n + 1) * (2 * n + 1)) / 6
 
 let difference_of_squares n = (square_of_sum n) - (sum_of_squares n)
