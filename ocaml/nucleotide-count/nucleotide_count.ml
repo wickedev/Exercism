19dba7a9c57f48ba24f4fb9e34d013b625256b94
wickedev <orange881217@gmail.com>
2022-07-29 15:13:55 +0900

    fix nucleotide-count indent

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -3,11 +3,10 @@ open Base
 let empty = Map.empty(module Char)
 
 
-
-let count_nucleotide s c = 
+let count_nucleotide ~s ~c = 
   if not (String.contains "ACGT" c)
     then Result.Error c
-else match (String.find s ~f:(fun c -> not (String.contains "ACGT" c))) with
+  else match (String.find s ~f:(fun c -> not (String.contains "ACGT" c))) with
     | None -> Result.Ok (String.count s ~f:(Char.equal c))
     | Some x -> Result.Error x
 
