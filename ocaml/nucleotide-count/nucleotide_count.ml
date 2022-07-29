5107032dadd54554a5a8905016eb839538800e36
wickedev <orange881217@gmail.com>
2022-07-29 15:01:35 +0900

    ocaml solved nucleotide-count

---
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -0,0 +1,15 @@
+open Base
+
+let empty = Map.empty(module Char)
+
+
+
+let count_nucleotide s c = 
+  if not (String.contains "ACGT" c)
+    then Result.Error c
+else match (String.find s ~f:(fun c -> not (String.contains "ACGT" c))) with
+    | None -> Result.Ok (String.count s ~f:(Char.equal c))
+    | Some x -> Result.Error x
+
+
+let count_nucleotides s = Result.Ok empty
