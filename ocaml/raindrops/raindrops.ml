02b3cf4b1af51b07332d70e4f5da5b6aaca20af8
wickedev <orange881217@gmail.com>
2022-08-01 15:03:34 +0900

    solved raindrop

---
+++ ocaml/raindrops/raindrops.ml
@@ -0,0 +1,9 @@
+
+let raindrop n =
+    let div by = n mod by = 0 in
+    let printer message by  = if div by then message else "" in
+    let output = (printer "Pling" 3)
+               ^ (printer "Plang" 5)
+               ^ (printer "Plong" 7) in
+    if output = "" then Int.to_string n else output
+    
