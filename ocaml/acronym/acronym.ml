60a83c79322e73975f5e7c4272238639a6f67e16
wickedev <orange881217@gmail.com>
2022-08-03 00:53:10 +0900

    ocaml acronym refectoring

--- ocaml/acronym/acronym.ml
+++ ocaml/acronym/acronym.ml
@@ -1,11 +1,16 @@
 module String = struct
   include String
-  let first s =
-    String.get s 0
+  let first (s: string): char = String.get s 0
 end
 
 let acronym text = text
   |> Str.split (Str.regexp "[ \n\r\x0c\t_-]+") 
   |> List.map String.first
   |> List.map Char.uppercase_ascii
-  |> Base.String.of_char_list
+  |> Base.String.of_char_list 
+
+(*let acronym text = Base.String.of_char_list
+  @@ List.map Char.uppercase_ascii
+  @@ List.map String.first
+  @@ Str.split (Str.regexp "[ \n\r\x0c\t_-]+") 
+  @@ text*)
